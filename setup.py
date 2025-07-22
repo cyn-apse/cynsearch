c9fc7d9364d3dc9fe8c60e9230dad952f2440666
Cynapse <cynapse.lxix@gmail.com>
2025-07-22 19:13:53 +0500

    Update setup.py
    getting it ready for PyPI

--- setup.py
+++ setup.py
@@ -1,11 +1,17 @@
 from setuptools import setup, find_packages
 
+with open("README.md", "r", encoding="utf-8") as f:
+    long_description = f.read()
+
 setup(
     name="cynsearch",
     version="0.1.0",
     description="A blazing-fast learned search library for chaotic datasets",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     author="Cynapse ψ∆Ξ",
     license="MIT",
+    url="https://github.com/cyn-apse/cynsearch",
     packages=find_packages(),
     install_requires=[
         "numpy>=1.24",
@@ -16,6 +22,8 @@ setup(
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
+        "Topic :: Software Development :: Libraries",
+        "Topic :: Scientific/Engineering :: Artificial Intelligence",
     ],
     python_requires=">=3.8",
     include_package_data=True,
